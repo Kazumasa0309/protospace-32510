f151a248207ae9218f3532b1b086d600c696f152
Kazumasa0309 <on89.uchiyama.kazumasa@gmail.com>
2020-12-15 11:25:55 +0900

    投稿コメント表示

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -19,6 +19,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = Comment.all
   end
 
   def edit
